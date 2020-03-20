23d8469fb6200ce994343e45afb9957cedf4ba99
Tony <haitrieu.nguyen@foodycorp.vn>
2020-03-20 13:40:12 +0700

    backfrom home button

--- polls/views.py
+++ polls/views.py
@@ -17,8 +17,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    result = "You're looking at the results of question %s."  % question_id
+    return render(request, 'polls/result.html', {'result': result})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
